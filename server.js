6180a471e2f05853f82b34a457862e0d7a6dd331
MaksimKosyanchuk <m.o.kosianchuk@student.khai.edu>
2024-04-05 02:41:50 +0300

    UnComment

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const port = process.env.PORT
 app.use(cors())
 app.use(express.json({ extended: true }))
 
-// app.use('/api/posts', require('./routes/posts.routes'))
-// app.use('/api/users', require('./routes/users.routes'))
-// app.use('/api/auth', require('./routes/auth.routes'))
-// app.use('/api/profile', require('./routes/profile.routes'))
-// app.use('/api', require('./routes/default.routes'))
+app.use('/api/posts', require('./routes/posts.routes'))
+app.use('/api/users', require('./routes/users.routes'))
+app.use('/api/auth', require('./routes/auth.routes'))
+app.use('/api/profile', require('./routes/profile.routes'))
+app.use('/api', require('./routes/default.routes'))
 app.use('/', require('./routes/default.routes'))
 
 const start = async () => {
     try {
-        // await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lccalb5.mongodb.net/?retryWrites=true&w=majority`)
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lccalb5.mongodb.net/?retryWrites=true&w=majority`)
         app.listen(port, () => console.log('server started on port: ', port))
     }
     catch (e) { 
