a57462258941a9820e3d6a5ce99769e3f2f55ce3
MaksimKosyanchuk <m.o.kosianchuk@student.khai.edu>
2024-04-05 15:46:56 +0300

    Added route and service for save post

--- services/profile.services.js
+++ services/profile.services.js
@@ -1,6 +1,8 @@
 const { get_jwt_token} = require('./auth.services')
 const { get_user } = require('./users.services')
-
+const { get_posts } = require('./posts.services')
+const { Types } = require('mongoose');
+const User = require('../models/User')
 
 async function get_profile(token) {
     if(!token) {
@@ -37,6 +39,75 @@ async function get_profile(token) {
     }
 }
 
+async function __save_post(user, post) {
+    try {
+        const result = await User.findOneAndUpdate(
+            { _id: user._id },
+            { $push: { saved_posts: post._id }});
+
+        return {
+            status: true,
+            message: "Post has been saved",
+            data: { post: post }
+        }
+    } catch (error) {
+        return {
+            status: false,
+            message: "Failed to save post",
+            data: { post: post }
+        };
+    }
+    
+}
+
+async function __unsave_post(user, post) {
+    try {
+        const result = await User.findOneAndUpdate(
+            { _id: user._id },
+            { $pull: { saved_posts: post._id }});
+
+        return {
+            status: true,
+            message: "Post has been unsaved",
+            data: { post: post }
+        }
+    } catch (error) {
+        return {
+            status: false,
+            message: "Failed to unsave post",
+            data: { post: post }
+        };
+    }
+}
+
+async function save_post(token, post_id) {
+    const user = await get_profile(token)
+
+    if(!user.status) {
+        return user
+    }
+    
+    if(!post_id) {
+        return {
+            status: false,
+            message: '"post_id" is null',
+            data: null
+        }
+    }
+
+    const posts = await get_posts(query = {  "_id": post_id })
+
+    if(!posts.status) {
+        return posts
+    }
+
+    const post = posts.data[0]
+    const result = user.data.saved_posts.some(savedPost => savedPost.equals(post._id))  ? await __unsave_post(user.data, post) : await __save_post(user.data, post)
+
+    return result
+}
+
 module.exports = {
-    get_profile
+    get_profile,
+    save_post
 }
