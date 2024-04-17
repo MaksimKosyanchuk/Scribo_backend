e733090b85b2542af86225c2e310b0a370b7d922
MaksimKosyanchuk <m.o.kosianchuk@student.khai.edu>
2024-04-17 21:08:17 +0300

    Added cors options to server.js

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 require('dotenv').config()
+
 const port = process.env.PORT
 
-app.use(cors())
+const corsOptions ={
+   origin:'*',
+   credentials:true,
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json({ extended: true }))
 
 app.use('/api/posts', require('./routes/posts.routes'))
