c4315d31a1491e2cbc3a090bbd96b67d73a98f68
MaksimKosyanchuk <m.o.kosianchuk@student.khai.edu>
2024-05-02 17:10:37 +0300

    Fixed upload serveice when URL is null

--- services/upload.services.js
+++ services/upload.services.js
@@ -1,29 +1,38 @@
 const Post = require('../models/Post')
 
 async function upload_image(img) {
-    const formData = new FormData();
-    formData.append('key', process.env.UPLOAD_IMAGE_API_KEY);
-    formData.append('image', img)
-    const response = await fetch(process.env.UPLOAD_IMAGE_URL, {
-        method: 'POST',
-        body: formData
-    });
-
-    const data = await response.json();
-    if(data.success) {
-        return {
-            status: true,
-            message: "Success uploaded image",
-            data: 
-            { 
-                url: data.data.url
+    try{
+        const formData = new FormData();
+        formData.append('key', process.env.UPLOAD_IMAGE_API_KEY);
+        formData.append('image', img)
+        const response = await fetch(process.env.UPLOAD_IMAGE_URL, {
+            method: 'POST',
+            body: formData
+        });
+        
+        const data = await response.json();
+        if(data.success) {
+            return {
+                status: true,
+                message: "Success uploaded image",
+                data: 
+                { 
+                    url: data.data.url
+                }
+            }
+        }
+        else {
+            return {
+                status: false,
+                message: data.error.message,
+                data: null
             }
         }
     }
-    else {
+    catch(e){
         return {
             status: false,
-            message: data.error.message,
+            message: e,
             data: null
         }
     }
